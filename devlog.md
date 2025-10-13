cc649db711842f24c3d40f7eca9f4558cc6a0a1a
Daniel A Gutierrez <22825631+Daniel-A-Gutierrez@users.noreply.github.com>
2025-10-13 12:32:07 -0700

    Gotta Audit

--- devlog.md
+++ devlog.md
@@ -10,3 +10,36 @@ project of its own.
 3. axum has changed how it works so now I'm using hyper::serve
 4. axum got rid of spa for some reason so im using tower_http::ServeDir
 5. it needs to serve from the root or else there's problems
+
+DEPLOYING
+1. cross compiling to linux doesnt really work so instead we need to install cargo and node on our vm
+2. Login to the vm as the root (sysadmin basically)
+    ssh root@ipaddress
+3. it'll prompt for a password. it won't show. type that in.
+4. we need to create a user and give them sudo permission.
+   adduser username
+    *it should prompt for a password, make it strong
+   usermod -aG sudo username
+5. ~~before we logout from root, lets install cargo~~ jk that'll only work for root if we do it now, do it after logging in as the new user
+    curl https://sh.rustup.rs -sSf | sh
+6. logout from root and login as the user name using ssh username@ipaddress and the new password
+7. ~~we need to install nodejs~~ skip this step, upload the whole node modules folder, it'll be faster. If you choose to ignore this advice, download the tarball manually and copy paste its contents according to resource 2. 
+    ~~sudo apt install nodejs~~
+    ~~sudo apt install npm~~
+    ~~ sudo npm install --global yarn~~
+8. set the port to 8080 (for now)  and the port to 0.0.0.0
+8. now use winscp to put your project files on the machine,~~ initialize the frontend~~ and build the backend.
+    ~~in the 'frontend' folder : "yarn install"~~
+    cargo build -r 
+9. it should be reachable from the outside world now. 
+10. later we have to configure a systemctl to launch it at startup but yea, thats all for now. 
+
+
+
+resources 
+https://www.redhat.com/sysadmin/install-apache-web-server
+
+https://stackoverflow.com/questions/57957973/npm-cannot-find-module-semver-after-reinstall
+
+for putting a gui on it : https://www.youtube.com/watch?v=ODhGNe0s4lI
+
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,72 @@ use axum::{self,routing::{get,post}};
 use tower_http;
 use tokio;
 use tower_http::services::ServeDir;
+use anyhow::{Result, ensure};
+use rusqlite::{Connection, self, OptionalExtension};
+
+#[cfg(debug_assertions)]
+const BIND : [u8;4] = [127,0,0,1];
+#[cfg(debug_assertions)]
+const PORT : u16 = 3334;
+// release build will be out-facing
+#[cfg(not(debug_assertions))]
+const BIND : [u8;4] = [0,0,0,0];
+#[cfg(not(debug_assertions))]
+const PORT : u16 = 80; // or 443 for https if we cert up
+
 
 #[tokio::main]
 async fn main() 
 {
-    let port = 3334;
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let addr = SocketAddr::from((BIND, PORT)); //0.0.0.0 for outfacing
     let page_router = axum::Router::new()
         .route("/hello", get( || async {"hello"}) )
         .nest_service("/",ServeDir::new("frontend/dist"))
         .into_make_service();
     let _server = hyper::Server::bind(&addr).serve(page_router).await.unwrap();
+}
+
+async fn init_db() -> Result<Connection>
+{
+    let conn = Connection::open_in_memory()?;
+    conn.execute("CREATE TABLE IF NOT EXISTS storage (id : INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    keys : UNIQUE TEXT, values : TEXT);
+                  CREATE UNIQUE INDEX IF NOT EXISTS idx_storage_keys ON storage (keys); ", ())?;
+    return Ok(conn);
+}
+
+async fn db_truncate(conn : &Connection, to : usize) -> Result<()>
+{
+    conn.execute("DELETE FROM storage WHERE id > $1;", (to,))?;
+    return Ok(());
+}
+
+async fn db_delete(conn : &Connection, key : &str) -> Result<()>
+{
+    conn.execute("DELETE FROM storage WHERE key = $1;", (key,))?;
+    return Ok(());
+}
+
+enum InsertError { Oversized, KeyAlreadyPresent, DBError(rusqlite::Error) }
+
+async fn db_insert<T>(conn : &Connection, key : &str, val : &str) -> Result<() , InsertError>
+{
+    if key.len() > 10_000 || val.len() > 10_000 { return Err(InsertError::Oversized) };
+
+    conn.execute("INSERT INTO storage ($1,$2);", (key, val)).map_err(
+        |e| {
+            match e.sqlite_error_code() {
+                Some(rusqlite::ErrorCode::ConstraintViolation) => InsertError::KeyAlreadyPresent, 
+                _ => InsertError::DBError(e),
+            }
+        })?;
+    return Ok(());
+}
+
+async fn db_get(conn : &Connection, key : &str) -> Result<Option<(usize, String)>, rusqlite::Error>
+{
+    return conn.query_one("SELECT id,value FROM storage WHERE key = $1;" , (key,) , 
+        |row| Ok(Some((row.get(0).expect("Storage table has incorrect columns"), 
+                  row.get(2).expect("Storage table has incorrect columns")))));
+            
 }
